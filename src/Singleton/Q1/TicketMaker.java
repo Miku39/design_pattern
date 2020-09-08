4b35cee4e49a8f3fc807c9c6170980ce44595974
Miku Sano <miku39.sano@gmail.com>
2020-09-09 08:28:27 +0900

    solved(Singleton) Q1

--- src/Singleton/Q1/TicketMaker.java
+++ src/Singleton/Q1/TicketMaker.java
@@ -1,6 +1,19 @@
 public class TicketMaker {
     private int ticket = 1000;
-    public int getNextTicketNumber() {
+    // 唯一のインスタンス
+    private static TicketMaker singleton = new TicketMaker();
+
+    private TicketMaker() {
+        System.out.println("インスタンスを生成しました。");
+    }
+    
+    public static TicketMaker getInstance() {
+        return singleton;
+    }
+
+    // 複数のスレッドから呼び出されても正しく動くようにするため
+    // synchronizedをつけている
+    public synchronized int getNextTicketNumber() {
         return ticket++;
     }
 }
