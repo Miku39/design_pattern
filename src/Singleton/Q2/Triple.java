585d7ff9c366dca7a9ada369425ea0e34c3866fc
Miku Sano <miku39.sano@gmail.com>
2020-09-10 19:57:45 +0900

    solved(Singleton) Q2

---
+++ src/Singleton/Q2/Triple.java
@@ -0,0 +1,20 @@
+public class Triple {
+
+    // クラスフィールド
+    // staticがついていないと無限ループになる
+    private static Triple[] triple = new Triple[] { new Triple(0), new Triple(1), new Triple(2), };
+    private int id;
+
+    private Triple(int id) {
+        System.out.println("The instance " + id + " is created.");
+        this.id = id;
+    }
+
+    public static Triple getInstance(int id) {
+        return triple[id];
+    }
+
+    public String toString() {
+        return "[Triple id=" + id + "]";
+    }
+}
