100dcf6dd62ddcb37e975cd3c37be2b8d9a57338
Miku Sano <miku39.sano@gmail.com>
2020-09-13 10:32:17 +0900

    solved(Builder): Q1

---
+++ src/Builder/Q1/Builder.java
@@ -0,0 +1,9 @@
+public interface Builder {
+    public abstract void makeTitle(String title);
+
+    public abstract void makeString(String str);
+
+    public abstract void makeItems(String[] items);
+
+    public abstract void close();
+}
