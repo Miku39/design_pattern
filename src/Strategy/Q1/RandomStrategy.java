6aefca9ee5edce7aa94e697734e3a0f46b1554c3
Miku Sano <miku39.sano@gmail.com>
2020-09-21 22:29:40 +0900

    solved(Strategy): Q1

---
+++ src/Strategy/Q1/RandomStrategy.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class RandomStrategy implements Strategy {
+    private Random random;
+
+    public RandomStrategy(int seed) {
+        random = new Random(seed);
+    }
+
+    public Hand nextHand() {
+        return Hand.getHand(random.nextInt(3));
+    }
+
+    public void study(boolean win) {
+    }
+}
