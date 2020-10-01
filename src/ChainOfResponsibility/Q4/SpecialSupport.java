cd6fe3dd705f65e82817aceacff18ec2291e7a5a
Miku Sano <miku39.sano@gmail.com>
2020-10-01 20:12:09 +0900

    solved(ChainOfResponsibility): Q4

---
+++ src/ChainOfResponsibility/Q4/SpecialSupport.java
@@ -0,0 +1,16 @@
+public class SpecialSupport extends Support {
+    private int number;
+
+    public SpecialSupport(String name, int number) {
+        super(name);
+        this.number = number;
+    }
+
+    protected boolean resolve(Trouble trouble) {
+        if (trouble.getNumber() == number) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/ChainOfResponsibility/Q4/Trouble.java
@@ -0,0 +1,15 @@
+public class Trouble {
+    private int number;
+
+    public Trouble(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String toString() {
+        return "[Trouble " + number + "]";
+    }
+}
