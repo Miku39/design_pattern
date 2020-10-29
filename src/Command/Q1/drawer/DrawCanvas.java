2cf24781a55c51e7d886c429c8789f16968bf66b
Miku Sano <miku39.sano@gmail.com>
2020-10-29 20:12:29 +0900

    feat(Command): Q1

--- src/Command/Q1/drawer/DrawCanvas.java
+++ src/Command/Q1/drawer/DrawCanvas.java
@@ -9,9 +9,9 @@ import javax.swing.*;
 
 public class DrawCanvas extends Canvas implements Drawable {
     // 描画色
-    private Color color = Color.red;
+    private Color color;
     // 描画する点の半径
-    private int radius = 6;
+    private int radius;
     // 履歴
     private MacroCommand history;
 
@@ -20,6 +20,7 @@ public class DrawCanvas extends Canvas implements Drawable {
         setSize(width, height);
         setBackground(Color.white);
         this.history = history;
+        init();
     }
 
     // 履歴全体を再描画
@@ -27,10 +28,21 @@ public class DrawCanvas extends Canvas implements Drawable {
         history.execute();
     }
 
+    public void init() {
+        color = Color.red;
+        radius = 6;
+        history.append(new ColorCommand(this, color));
+    }
+
     // 描画
     public void draw(int x, int y) {
         Graphics g = getGraphics();
         g.setColor(color);
         g.fillOval(x - radius, y - radius, radius * 2, radius * 2);
+    }
+
+    // 色
+    public void setColor(Color color) {
+        this.color = color;
     }
 }
