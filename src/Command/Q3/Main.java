94b73cd44d5ebbdf6c487741c51df3fd9aac3315
Miku Sano <miku39.sano@gmail.com>
2020-11-03 16:29:39 +0900

    feat(Command): Q3

--- src/Command/Q3/Main.java
+++ src/Command/Q3/Main.java
@@ -5,7 +5,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Main extends JFrame implements ActionListener {
+public class Main extends JFrame implements ActionListener, MouseMotionListener, WindowListener {
     // 描画履歴
     private MacroCommand history = new MacroCommand();
     // 描画領域
@@ -17,18 +17,8 @@ public class Main extends JFrame implements ActionListener {
     public Main(String title) {
         super(title);
 
-        this.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
-        canvas.addMouseMotionListener(new MouseMotionAdapter() {
-            public void mouseDragged(MouseEvent e) {
-                Command cmd = new DrawCommand(canvas, e.getPoint());
-                history.append(cmd);
-                cmd.execute();
-            }
-        });
+        this.addWindowListener(this);
+        canvas.addMouseMotionListener(this);
         clearButton.addActionListener(this);
 
         Box buttonBox = new Box(BoxLayout.X_AXIS);
@@ -48,6 +38,39 @@ public class Main extends JFrame implements ActionListener {
             history.clear();
             canvas.repaint();
         }
+    }
+
+    // MouseMotionListener用
+    public void mouseMoved(MouseEvent e) {
+    }
+
+    public void mouseDragged(MouseEvent e) {
+        Command cmd = new DrawCommand(canvas, e.getPoint());
+        history.append(cmd);
+        cmd.execute();
+    }
+
+    // WindowListener用
+    public void windowClosing(WindowEvent e) {
+        System.exit(0);
+    }
+
+    public void windowActivated(WindowEvent e) {
+    }
+
+    public void windowClosed(WindowEvent e) {
+    }
+
+    public void windowDeactivated(WindowEvent e) {
+    }
+
+    public void windowDeiconified(WindowEvent e) {
+    }
+
+    public void windowIconified(WindowEvent e) {
+    }
+
+    public void windowOpened(WindowEvent e) {
     }
 
     public static void main(String[] args) {
